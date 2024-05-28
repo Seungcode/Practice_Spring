ef14e3e6d47ed5959ecaa7299e20297cdb7cbe2d
Seungcode <ehrehrdltkfk@gmail.com>
2024-05-28 16:05:03 +0900

    SOLID고려 변경

--- src/main/java/hello/core/Order/OrderServiceImpl.java
+++ src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -2,15 +2,26 @@ package hello.core.Order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    //인터페이스에만 의존하기 위한 변 -> 오잉 그러면 전 뭐가 되는 걸까 -> 누군가 구현 객체를 알아서 생성하고 주입해줘야한다! -> AppConfig
+    private final DiscountPolicy discountPolicy;
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    /*
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    위와 같이 사용항 경우 SOLID 중 개방-폐쇄 원칙과 의존관계 역전 원칙을 지키지 못함
+    -> 바꾸려면 결국 이 부분을 건드려야 하기 때문 -> 즉 인터페이스가 타 인터페이스의 구현까지 의존하는 상태임
+    */
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
